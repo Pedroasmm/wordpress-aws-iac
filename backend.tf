0dfb312dae21303bcfc3c4919e292337f5bfa4fb
Pedroasmm <pedroasmmarcelino@gmail.com>
2025-03-31 19:41:26 +0100

    Remove large files from the repo history and add .gitignore

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-aws-iac-bucket"  # Replace with your bucket name
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-    # dynamodb_table = "terraform-lock"  # If using DynamoDB for state locking
-  }
-}
--- variables.tf
+++
@@ -1,5 +0,0 @@
-# variables.tf or main.tf
-variable "aws_region" {
-  description = "The AWS region to create resources in"
-  type        = string
-}
